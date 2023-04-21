a87a3137eaa953c0a59937bdab74e669d2d82701
Leandro <leandrodev83@gmail.com>
2023-04-20 21:55:15 -0300

    Criando os arquivos principais e a barra de menu lateral co seus links, tabém dando inicil ao top menu

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/navigation.css">
+    <link rel="stylesheet" href="assets/css/top-menu.css">
+</head>
+
+<body class="d-flex" style="background-color: #eee;">
+    <?php
+    include("includes/navigation.php");
+    ?>
+
+    <?php
+    include("includes/top-menu.php");
+    ?>
+
